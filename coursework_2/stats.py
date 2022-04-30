a570ff85db745bb2d555e6f1c8ae346920faf614
Mukhammad <66946817+jjEnokenti@users.noreply.github.com>
2022-04-30 15:20:42 +0300

    Update stats.py

--- coursework_2/stats.py
+++ coursework_2/stats.py
@@ -7,15 +7,15 @@ class Stats:
     self._name - имя игрока для которого считается статистика
     """
 
-    def __init__(self, name, count_subwords):
-        self._name = name
+    def __init__(self, player, count_subwords):
+        self._name = player
         self._count_subwords = count_subwords
 
     def __repr__(self):
         count = self._name.quantity_words
         return f'Игра закончилась!\n' \
                f'Вы угадали {count} {change_word_ending("слов", count)} из {self._count_subwords}'
-    
+
     def save_history(self):
         with open('history/history.txt', 'a+', encoding='utf-8') as outf:
             print(f'У {self._name} отгаданных слов {self._name.quantity_words}', file=outf)
